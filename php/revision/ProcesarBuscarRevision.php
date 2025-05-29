7339ee4e6bb18d1d657bcd0a62241304d8317e52
Matías Flores <matiasflores120402@gmail.com>
2025-05-29 20:44:20 -0300

    Se agregó punto 4

---
+++ php/revision/ProcesarBuscarRevision.php
@@ -0,0 +1,73 @@
+<?php
+include '../conexion.php';
+
+$fingreso = $_POST['fingreso'];
+$fegreso = $_POST['fegreso'];
+
+$sql = "SELECT revision.cod_revision, revision.fingreso, revision.fegreso,
+                revision.estado, revision.cambio_filtro, revision.cambio_aceite,
+                revision.cambio_freno, revision.descripcion,
+                auto.marca, auto.modelo
+        FROM revision
+        INNER JOIN auto
+        ON revision.cod_auto = auto.cod_auto
+        WHERE fingreso BETWEEN '$fingreso' AND '$fegreso'
+        ";
+
+$resultado = mysqli_query($conexion, $sql);
+$cantidad = mysqli_num_rows($resultado);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Revisiones Encontradas</h2>
+
+    <?php if ($cantidad > 0): ?>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Fecha ingreso</th>
+                    <th>Fecha egreso</th>
+                    <th>Estado</th>
+                    <th>Filtro</th>
+                    <th>Aceite</th>
+                    <th>Freno</th>
+                    <th>Descripción</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($revision = mysqli_fetch_assoc($resultado)): ?>
+                    <tr>
+                        <td><?php echo $revision['cod_revision'] ?></td>
+                        <td><?php echo $revision['fingreso'] ?></td>
+                        <td><?php echo $revision['fegreso'] ?></td>
+                        <td><?php echo $revision['estado'] ?></td>
+                        <td><?php echo $revision['cambio_filtro'] ?></td>
+                        <td><?php echo $revision['cambio_aceite'] ?></td>
+                        <td><?php echo $revision['cambio_freno'] ?></td>
+                        <td><?php echo $revision['descripcion'] ?></td>
+                        <td><?php echo $revision['marca'] ?></td>
+                        <td><?php echo $revision['modelo'] ?></td>
+                    </tr>
+                <?php endwhile ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No hay revisiones registradas en esas fechas.</p>
+    <?php endif ?>
+        <br>
+        <a href="../../menu.html">Volver</a>
+</body>
+
+</html>
